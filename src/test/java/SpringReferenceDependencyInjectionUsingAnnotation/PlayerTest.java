89a2a2eb0b4892bcbac1e261a43cd7923573a125
rahul mendiratta <rahulmendiratta13@gmail.com>
2020-03-10 14:41:27 +0530

    Added Intrspector N otes

--- src/test/java/SpringReferenceDependencyInjectionUsingAnnotation/PlayerTest.java
+++ src/test/java/SpringReferenceDependencyInjectionUsingAnnotation/PlayerTest.java
@@ -7,6 +7,13 @@ public class PlayerTest {
   public static void main(String[] args) {
     ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationContextVariableDIAnnotation.xml");
 
+    /*Note :- Spring Bean using Java Bean Introspector to decapatialise the class Name Passed
+    Like :- Class Name = Varvade
+            java.beans.Introspector.decapatalise("Varvade");
+            O/P :- varvade
+
+     If first two character are capital then Introspector will not change the class name.
+     */
     Player player = classPathXmlApplicationContext.getBean("varvade",Player.class);
 
     String game = player.game();
